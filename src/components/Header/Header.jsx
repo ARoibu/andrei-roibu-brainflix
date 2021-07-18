93e91196a570be8aebe573f3b058f184734e7257
ARoibu <aroibu@uwo.ca>
2021-07-18 15:57:18 -0400

    styled mobile header and video poster

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import logo from '../../assets/images/Logo/Logo-brainflix.svg'
+import './Header.scss'
 
 function header() {
     return (
-        <header>
-            <img src={logo} alt="BrainFlix Logo" />
-            <br/>
-            <input type='text'/>
-            <br/>
-            <button>+ Upload</button>
-            <div></div>
+        <header className='header'>
+            <img className='header__logo' src={logo} alt="BrainFlix Logo" /> 
+            <input placeholder='Search' className='header__search' type='text'/>
+            <button className='header__button'>UPLOAD</button>
+            <div className='header__mohan'></div>
         </header>
     );
 }
