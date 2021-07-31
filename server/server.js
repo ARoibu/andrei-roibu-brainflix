cd0b4c166c45ae7808009786922fa8f9a7f0dc80
ARoibu <aroibu@uwo.ca>
2021-07-31 02:07:32 -0400

    added and imported route file/folder. created public folder and added all video images

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
+const routes = require('./routes/routes')
+const cors = require("cors");
 
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.send('Express is running!');
 });
 
-app.listen(8080, () => {
-    console.log('Server Started on http://localhost:8080');
+app.use("/", routes);
+
+app.listen(PORT, () => {
+    console.log(`Server Started on http://localhost:${PORT}`);
     console.log('Press CTRL + C to stop server');
 })
